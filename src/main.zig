9729cdf5bf740a8ade899c15e86db31dd87eb897
NetDefender <danielsith@hotmail.com>
2024-01-05 04:09:40 +0100

    feat: for while

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,21 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    try stdout_file.print("Without buffering:\n", .{});
+    var name: [5]u8 = undefined;
+    const hello = [_]u8{ 'H', 'e', 'l', 'l', 'o' };
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    for (hello, 0..) |c, i| {
+        name[i] = c;
+    }
 
-    try bw.flush();
+    var i: i32 = 4;
+
+    while (i >= 0) : (i -= 1) {
+        try stdout_file.print("{d}\n", .{i});
+    }
+
+    try stdout_file.print("{s} has len {d}\n", .{ name, name.len });
 }
 
 test "simple test" {
